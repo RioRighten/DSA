9c34d1fb3c6c4dde43335ff1af77379b12a4b32f
Rio Righten <143250959+RioRighten@users.noreply.github.com>
2024-08-28 17:18:04 +0000

    X

--- Queue/Queue.c
+++ Queue/Queue.c
@@ -23,7 +23,7 @@ void Insert_queue()
     }
 }
 
-/* Function to perform deletion operation */
+/* Function to perform Deletion Operation */
 
 void Delete_queue(){
     if(front == rear){
@@ -33,5 +33,43 @@ void Delete_queue(){
         front++;
         ch=q[front];
         printf("\n Element deleted: %d",ch);
+    }
+}
+
+/* Function to show Output*/
+
+void Display_queue(){
+    int i;
+    if(front == rear){
+        printf("\n Underflow");
+    }
+    else{
+        for(i=front+1; i<=rear;i++)
+            printf("%d",q[i]);
+    }
+}
+
+
+void main(){
+    int choice;
+    while(1){
+        printf("\n Insert -> 1 \n Delete -> 2 \n Display -> 3 \n Quit -> 4");
+        printf("\n Enter the choice:");
+        scanf("%d",&choice);
+        switch (choice)
+        {
+        case 1:
+            Insert_queue();
+            break;
+        case 2:
+            Delete_queue();
+            break;
+        case 3:
+            Display_queue();
+            break;
+        default:
+            exit(0);
+        }
+
     }
 }
